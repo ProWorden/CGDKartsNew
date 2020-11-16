aa1b434fc66fa0eb588d24cbb4c9a009287d4d24
Unknown <Christopher2.Worden@live.uwe.ac.uk>
2020-11-16 18:24:19 +0000

    Working crash mechanics for p1

--- DisciplesKart/Assets/Scripts/CornerScript.cs
+++ DisciplesKart/Assets/Scripts/CornerScript.cs
@@ -9,36 +9,68 @@ public class CornerScript : MonoBehaviour
     public player1 P1;
     private bool player1follow = true;
     public bool bollision = false;
-    
+    public float timer = 0;
+    public Vector3 RespawnPoint = new Vector3(0,0,0);
+    private bool setRespawn = false;
+    public GameObject playerOne;
 
-   private void OnTriggerStay(Collider other)
+
+    private void OnTriggerStay(Collider other)
     {
        
      if (other.tag == "Kart1")
         {
             bollision = true;
-            if (P1.current_speed >= maxCornerSpeed)
+            if (P1.current_speed >= maxCornerSpeed && player1follow && !setRespawn)
             {
                 player1follow = false;
             }
         }
     }
 
-    private void Update()
+    private void LateUpdate()
     {
         FallenOff();
+        Respawn();
     }
 
     void FallenOff()
     {
         if(player1follow == false)
         {
+            RespawnPoint =  playerOne.transform.position;
             AIPlayer1.enabled = false;
 
-            //when movement is in other cript to this one the velocity should be kept but it will fly in a straight line
+            //when movement in other script to this one the velocity should be kept but it will fly in a straight line
             P1.crashed = true;
 
-            //then call respawn after a timer
+            //then call respawn 
+            setRespawn = true;
+            player1follow = true;
+            
         }
+    }
+
+    void Respawn()
+    {
+        if(setRespawn)
+        {
+            timer += Time.deltaTime;
+
+            if (timer >= 1)
+            {
+                P1.current_speed = 0;
+                P1.crashed = false;
+
+                //teleport player to start of corner
+                playerOne.transform.position = RespawnPoint;
+                AIPlayer1.enabled = true;
+
+                //reset timer
+                timer = 0;
+                setRespawn = false;
+            }
+        }
+     
     }
 }
